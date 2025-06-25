29c672607e1a963be057c8de009a0177344d90d9
jo13han <koshy.johan@gmail.com>
2025-06-25 15:16:03 +0530

    suspense fix

--- ecommerce/src/app/products/page.tsx
+++ ecommerce/src/app/products/page.tsx
@@ -1,11 +1,17 @@
-'use client';
 import ProductsSection from '@/sections/Products/ProductsSection';
+import { Suspense } from 'react';
+
+function ProductsSkeleton() {
+  return <div>Loading...</div>;
+}
 
 export default function ProductsPage() {
   return (
     <div className="bg-white min-h-screen">
       <main className="flex-1">
-        <ProductsSection />
+        <Suspense fallback={<ProductsSkeleton />}>
+          <ProductsSection />
+        </Suspense>
       </main>
     </div>
   );
